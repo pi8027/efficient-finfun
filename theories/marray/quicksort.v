c2eda705294db277e0b0f1904ae1a58315af1509
Kazuhiko Sakaguchi <pi8027@gmail.com>
2017-10-25 03:04:44 +0900

    done: quicksort.v

--- theories/marray/quicksort.v
+++ theories/marray/quicksort.v
@@ -143,9 +143,7 @@ Section Quicksort.
 
 Variable
   (I : finType) (A : Type) (cmp : A -> A -> bool)
-  (* Hcmp_refl : forall x, cmp x x *)
   (Hcmp_trans : forall x y z, cmp x y -> cmp y z -> cmp x z)
-  (* Hcmp_total : forall x y, cmp x y || cmp y x *)
   (Hcmp_asym : forall x y, ~~ cmp y x -> cmp x y).
 
 Definition swap (i j : 'I_#|I|) : AState [:: (I, A)] unit :=
@@ -303,4 +301,135 @@ Fixpoint quicksort_rec (i j : 'I_#|I|.+1) (n : nat) :
     | _, _ => fun _ => astate_ret tt
   end (erefl (i.+1 < j)).
 
+Definition quicksort : AState [:: (I, A)] unit :=
+  quicksort_rec ord0 ord_max #|I|.
+
+Lemma run_quicksort_rec (i j : 'I_#|I|.+1) (n : nat) (arr : {ffun I -> A}) :
+  j <= i + n ->
+  let: (_, arr', _) := run_AState (quicksort_rec i j n) (tt, arr) in
+  exists2 p : {perm I},
+    arr' = [ffun ix => arr (p ix)] &
+    perm_on [set ix | i <= fin_encode ix < j] p /\
+    (forall ix ix' : 'I_#|I|, i <= ix -> ix < ix' -> ix' < j ->
+                     cmp (arr (p (fin_decode ix))) (arr (p (fin_decode ix')))).
+Proof.
+have Hid_arr (arr' : {ffun I -> A}):
+  [ffun ix => arr' ((1%g : {perm I}) ix)] = arr'
+  by apply/ffunP => ix; rewrite ffunE permE.
+elim: n i j arr => [| n IH] i j arr; rewrite (addn0, addnS) => H /=;
+  first by
+    exists 1%g; rewrite ?Hid_arr ?perm_on1; split => // ix ix' H0 H1 H2;
+    move/(leq_trans H0)/(leq_trans H)/(leq_trans H2): (ltnW H1); rewrite ltnn.
+case: {2 3}(i.+1 < j) (erefl (i.+1 < j));
+  last by move/negbT; rewrite -leqNgt => H0; exists 1%g;
+    rewrite ?Hid_arr ?perm_on1; split => // ix ix'; rewrite -ltnS => H1 H2 H3;
+    move/(leq_trans H1)/(leq_trans H0)/(leq_trans H3): H2; rewrite ltnn.
+rewrite -/(is_true _) => /= H0; case: run_partition; first by apply ltnW.
+move => /= ppart k' H1.
+set k := Ordinal (quicksort_rec_subproof4 _ _).
+have Hk: k' = (@Ordinal #|I|.+1 k.+1 (ltn_ord k))
+  by apply/ord_inj => /=; case: (nat_of_ord k') H1.
+move: k Hk H1 => k Hk; subst k' => /=; rewrite ltnS /=.
+set i' := Ordinal (quicksort_rec_subproof1 _).
+have ->:
+  Ordinal (quicksort_rec_subproof3 (@Ordinal #|I|.+1 k.+1 (ltn_ord k))) =
+  Ordinal (leqW (ltn_ord k)) by apply/ord_inj => /=.
+set k' := Ordinal (leqW (ltn_ord k)).
+set sk := @Ordinal #|I|.+1 k.+1 (ltn_ord k).
+rewrite run_swap => H1 Hpart1 Hpart2 Hpart3.
+set arr' := [ffun _ => _].
+have {arr'} ->:
+  arr' = [ffun ix => arr (ppart (tperm (fin_decode i') (fin_decode k) ix))]
+  by apply/ffunP => ix; rewrite !ffunE.
+set arr' := [ffun _ => _].
+have/(IH i k' arr'): k' <= i + n
+  by rewrite -ltnS; case/andP: H1 H => _; apply leq_trans.
+case: (run_AState _ _) => [] [] [] arr'' _ [] pl -> {arr''} [] /= IHl1 IHl2.
+have{IH} /(IH sk j [ffun ix => arr' (pl ix)]): j <= sk + n
+  by apply/(leq_trans H); rewrite ltn_add2r /=; case/andP: H1.
+case: (run_AState _ _) => [] [] [] arr'' _ [] pr -> {sk arr''} [] /= IHr1 IHr2.
+exists (pr * pl * tperm (fin_decode i') (fin_decode k) * ppart)%g;
+  first by apply/ffunP => ix; rewrite !ffunE !permM.
+split => [| ixl ixr H2 H3 H4]; rewrite ?permM.
+- apply perm_onM;
+    last by apply/(subset_trans Hpart1)/subsetP => ix;
+            rewrite !inE => /andP [] /ltnW -> ->.
+  apply perm_onM;
+    last by apply/subsetP => ix; rewrite !inE; case: tpermP;
+            rewrite ?eq_refl // => -> {ix} _; rewrite fin_decodeK //= leqnn;
+            case/andP: H1; apply/leq_ltn_trans.
+  apply perm_onM; [ apply/(subset_trans IHr1) | apply/(subset_trans IHl1) ];
+    apply/subsetP => ix; rewrite !inE => /andP [].
+  + by case/andP: H1 => H1 _ /ltnW/(leq_trans H1) -> ->.
+  + by move => -> H2; case/andP: H1 => _; move: H2; apply ltn_trans.
+- case: (ltnP k ixl) => H5;
+    first by move: (IHr2 ixl ixr H5 H3 H4); rewrite !ffunE.
+  rewrite (out_perm IHr1) ?inE ?fin_decodeK 1?ltnNge ?H5 //.
+  case: (ltnP ixr k) => [H6 |];
+    first by rewrite (out_perm IHr1) ?inE ?fin_decodeK 1?ltnNge ?(ltnW H6) //;
+      move: (IHl2 ixl ixr H2 H3 H6); rewrite !ffunE.
+  rewrite leq_eqVlt; case/orP => [/eqP /ord_inj |] H6.
+  + subst ixr => {H5}.
+    rewrite (out_perm IHr1) ?(out_perm (x := fin_decode k) IHl1) ?tpermR
+            ?(out_perm (x := fin_decode i') Hpart1) ?inE ?fin_decodeK ?ltnn
+            ?andbF //; apply Hcmp_asym.
+    move: (Hpart2 (fin_encode (tperm (fin_decode i') (fin_decode k)
+                                     (pl (fin_decode ixl))))).
+    rewrite ltnS ffunE fin_encodeK; apply.
+    have: fin_decode ixl \in [set ix | i <= fin_encode ix < k]
+      by rewrite inE fin_decodeK H2.
+    rewrite // -(perm_closed _ IHl1) (_ : i = i' :> nat) // inE leq_eqVlt =>
+      /andP [] /orP [/eqP /ord_inj -> | H5 H6];
+      first by rewrite fin_encodeK tpermL fin_decodeK leqnn => ->.
+    by rewrite tpermD ?H5 ?(ltnW H6) //=
+               (can2_eq (@fin_decodeK _) (@fin_encodeK _))
+               -(inj_eq (@ord_inj _)) neq_ltn (H5, H6) // orbT.
+  + have: fin_decode ixr \in [set ix | k < fin_encode ix < j]
+      by rewrite inE fin_decodeK H6 H4.
+    rewrite -(perm_closed _ IHr1) => Hixr.
+    rewrite (out_perm (x := pr (fin_decode ixr)) IHl1); last by
+      move: Hixr; rewrite !inE negb_and -leqNgt => /andP[] /ltnW ->; rewrite orbT.
+    rewrite (tpermD (z := pr _)); try by
+      move: Hixr; rewrite inE => /andP [Hixr Hixr'];
+      rewrite (can2_eq (@fin_decodeK _) (@fin_encodeK _)) -(inj_eq (@ord_inj _))
+              /= neq_ltn ?Hixr // (leq_ltn_trans (leq_trans H2 H5) Hixr).
+    rewrite -(fin_encodeK (pr _)); move: Hixr; rewrite !inE;
+      move: {ixr H3 H4 H6} (fin_encode _) => ixr Hixr.
+    have: fin_decode ixl \in [set ix | i <= fin_encode ix <= k]
+      by rewrite inE fin_decodeK H2 H5.
+    rewrite -(perm_closed (s := pl) (fin_decode ixl)) => [Hixl |];
+      last by apply/(subset_trans IHl1)/subsetP => ix;
+              rewrite !inE => /andP [] -> /ltnW.
+    rewrite -(fin_encodeK (tperm _ _ _)).
+    set ixl' := fin_encode (tperm _ _ _).
+    have: i <= ixl' <= k by
+      move: Hixl; rewrite /ixl' inE;
+      case: tpermP => //= /(f_equal (fun i => nat_of_ord (@fin_encode _ i)));
+      rewrite !fin_decodeK /= => <-; rewrite !leqnn !andbT //=.
+    move: {ixl H2 H5 Hixl} (Hpart3 _ Hixr) ixl'; rewrite ffunE => Hcmpr ixl;
+    rewrite leq_eqVlt -(ltnS ixl k) andb_orl (_ : i = i' :> nat) //
+        => /orP [/andP [/eqP /ord_inj <- _ {ixl}] | /Hpart2]; first by
+      rewrite (out_perm Hpart1) ?inE ?fin_decodeK ?negb_and -?leqNgt ?leqnn.
+    by rewrite ffunE => /Hcmp_asym Hcmpl; apply (Hcmp_trans Hcmpl Hcmpr).
+Qed.
+
+CoInductive quicksort_spec (arr : {ffun I -> A}) :
+  unit * {ffun I -> A} * unit -> Prop :=
+  QuicksortSpec :
+    forall (p : {perm I}),
+      (forall ix ix' :
+                'I_#|I|, ix < ix' ->
+                cmp (arr (p (fin_decode ix))) (arr (p (fin_decode ix')))) ->
+      quicksort_spec arr (tt, [ffun ix => arr (p ix)], tt).
+
+Lemma run_quicksort (arr : {ffun I -> A}) :
+  quicksort_spec arr (run_AState quicksort (tt, arr)).
+Proof.
+move: (@run_quicksort_rec ord0 ord_max #|I| arr).
+rewrite /quicksort add0n leqnn => /(_ erefl).
+case: (run_AState _ _) => [] [] [] arr' [] [] p -> {arr'} [] _ H.
+by constructor => ix ix' Hix; apply H.
+Qed.
+
+End Quicksort.
 End Quicksort.
