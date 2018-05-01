a2e1d3b80fefa1be7902c2368e4324aa4dcfd0c3
Kazuhiko Sakaguchi <pi8027@gmail.com>
2018-05-01 15:49:46 +0900

    Update build scripts

--- ocaml/myocamlbuild.ml
+++ ocaml/myocamlbuild.ml
@@ -7,10 +7,14 @@ dispatch begin function
     end
   | After_rules ->
     begin
-      flag ["ocaml";"compile";"native";"dflambda"] (S [ A "-dflambda"]);
-      flag ["ocaml";"compile";"native";"drawflambda"] (S [ A "-drawflambda"]);
-      flag ["ocaml";"compile";"native";"dflambda-no-invariants"] (S [ A "-dflambda-no-invariants"]);
-      flag ["ocaml";"compile";"native";"dflambda-verbose"] (S [ A "-dflambda-verbose"]);
+      flag ["ocaml";"compile";"dparsetree"] (S [A "-dparsetree"]);
+      flag ["ocaml";"compile";"dtypedtree"] (S [A "-dtypedtree"]);
+      flag ["ocaml";"compile";"drawlambda"] (S [A "-drawlambda"]);
+      flag ["ocaml";"compile";"dlambda"] (S [A "-dlambda"]);
+      flag ["ocaml";"compile";"native";"dflambda"] (S [A "-dflambda"]);
+      flag ["ocaml";"compile";"native";"drawflambda"] (S [A "-drawflambda"]);
+      flag ["ocaml";"compile";"native";"dflambda-no-invariants"] (S [A "-dflambda-no-invariants"]);
+      flag ["ocaml";"compile";"native";"dflambda-verbose"] (S [A "-dflambda-verbose"]);
     end
   | _ -> ()
 end
